deea00edded2b4df75b4379cc1824630867a0d3a
florentin.marty <florentin.marty@scs.ch>
2020-05-26 20:51:14 +0200

    initial comit with some basic functionalities

---
+++ data/eventdata.js
@@ -0,0 +1,115 @@
+blogs = {
+"messages": [
+  {
+      "title": "Sensation",
+      "date": "12.6.2020",
+      "id": "1.1.1",
+      "character": "rathaus",
+      "name": "",
+      "event": "Ankündigung",
+      "phase": "1",
+      "endDisplay":"2",
+      "text": "Sensation! BuLa kommt nach Goms! Der Pfadfinderbund Schweiz hat entschieden das Bundeslager 2021 bei uns im schönen Goms stattfinden zu lassen. Ganz Goms freut sich riesig! <br><br>  Sensation! CaFé arrive à Goms! L'Association scoute suisse a décidé d'organiser le camp fédéral en 2021 dans la magnifique région de Goms. Tout les Goms sont vraiment content! <br><br> Sensation! CaFe sta arrivando a Goms! La Swiss Scout Association ha deciso di tenere il campo federale nel 2021 nella splendida Goms. Tutto Goms è davvero felice!",
+      "img": "",
+      "comments":""
+  },
+  {
+      "title": "BULA kommt nach Goms!!",
+      "date": "12.6.2020",
+      "id": "1.1.1",
+      "character": "mayor",
+      "name": "alfons affentranger",
+      "event": "Ankündigung",
+      "phase": "1",
+      "endDisplay":"2",
+      "text": "BuLa kommt nach Goms! Konnte wichtigste Leute und Chefpräsident von Pfadi überzeugen. 100 000 Leute kommen und unterstützen unser Gewerbe. Gute Leistung. #bestdealever #bestscouts #besterpräsidentenchef <br> #mova21 #cafe21 #bula21 #pfadi #scout #pfadiscout #gomslovesbula #gomso",
+      "img": "mayor_rathaus.jpg",
+      "comments":""
+  },
+  {
+      "title": "Tolle Sache!",
+      "date": "13.6.2020",
+      "id": "1.1.2",
+      "character": "Metzgerin",
+      "event": "Ankündigung",
+      "phase": "1",
+      "endDisplay":"2",
+      "text": "Was BULA in Goms? Super toll! Dann haben wir die Gelegenheit den Weltrekord des längsten Fleischspiess vom letzten Metzgerins Union Life Act zu schlagen. <br><br> Quelle BULA dans le Goms ? Super génial ! Nous avons alors l'occasion de battre le record mondial de la plus longue brochette de viande, établi lors du dernier Metzgerins Union Life Act. <br><br> Che BULA in Goms? Super grande! Poi abbiamo l'opportunità di battere il record mondiale per il più lungo spiedino di carne dell'ultimo Metzgerins Union Life Act.",
+      "img": "fleischspiess.jpg",
+      "comments":[
+        {
+            "character": "flickflack",
+            "text":"Haha liebe Conchita! Beim BuLA handelt es sich um das Bundeslager der Pfadibewegung Schweiz und nicht um diesen Mega-Event der Metzgerszunft, von dem Du immer schwärmst. <br><br> Ha ha cher Conchita ! Le Café est le camp fédéral du mouvement scout suisse et non ce méga-événement de la guilde des bouchers, dont vous ne cessez de parler. <br><br> Ha ha cara Conchita! Il CaFe è il campo federale del Movimento svizzero di scoutismo e non questo mega-evento della corporazione dei macellai, di cui si va sempre pazzi." ,
+            "date": "13.6.2020"
+        },
+        {
+            "character": "Metzgerin",
+            "text":"Uups...typisch für mich! Habe ich wohl verwechselt. Aber die Pfader essen wahrscheinlich auch gerne Würste.<br><br>Oups... c'est bien ma chance ! J'ai dû confondre. Mais les pathes aiment probablement aussi manger des saucisses.<br><br> Ops... che fortuna! Devo averlo confuso. Ma probabilmente anche ai pathes piace mangiare salsicce." ,
+            "date": "13.6.2020"
+        }
+      ]
+    }
+    ,
+    {
+        "title": "OMG thats just awesome!!",
+        "date": "13.6.2020",
+        "id": "1.1.3",
+        "character": "Influencer",
+        "event": "Ankündigung",
+        "phase": "1",
+        "endDisplay":"2",
+        "text": "<3 <3!! OMG! I love nature!",
+        "img": "influencer_rathaus.png",
+        "comments":[
+          {
+              "character": "Pfarrer",
+              "text":"Ich will reden von deiner herrlichen, schönen Pracht und von deinen Wundern. Psalm 145:5 / LUT",
+              "date": "13.6.2020"
+          }
+        ]
+      }
+      ,
+      {
+          "title": "So viel Müll?",
+          "date": "13.6.2020",
+          "id": "1.1.3",
+          "character": "Herr und Frau Goms",
+          "event": "Ankündigung",
+          "phase": "1",
+          "endDisplay":"2",
+          "text": "Wollen wir wirklich solche Zustände in unserem schönen Goms? <br><br> Voulons-nous vraiment de telles conditions dans notre beau Goms ?<br><br>Vogliamo davvero queste condizioni nel nostro bellissimo Goms?",
+          "img": "müllopenair.jpg",
+          "comments":[
+            {
+                "character": "jäger",
+                "text":"Steigt ebenfalls kritisch ein, da Naturschutz mit viel Jungwild gleich daneben.<br><br> Il s'agit également d'un point critique, car la protection de la nature est très présente, avec beaucoup de jeunes gibiers juste à côté.<br><br>Inoltre entra in gioco anche in modo critico, dato che la conservazione della natura con un sacco di gioco giovane proprio accanto ad essa.",
+                "date": "13.6.2020"
+            }
+          ]
+        }
+  ]
+};
+
+
+
+events = {"type": "FeatureCollection",
+"features": [
+  {
+    "type": "Feature",
+    "properties": {
+      "event": "Ankündigung",
+      "phase": "1",
+      "endDisplay":"2",
+      "icon": "Spatenstich.png"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        73.7949,
+        40.7282,
+        1
+      ]
+    }
+  }
+]
+};
